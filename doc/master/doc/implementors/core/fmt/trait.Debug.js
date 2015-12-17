80d86ba80569b33a4a75124800e1a70e099a4ea0
Simo Kinnunen (via Travis CI) <simo@shoqolate.com>
2015-12-17 13:52:07 +0000

    Rebuild pages at 2d91e8c0b388ea952d44d03c9a9b19872f3eaf31

---
+++ doc/master/doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['r2d2_redis'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='r2d2_redis/enum.Error.html' title='r2d2_redis::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='r2d2_redis/struct.RedisConnectionManager.html' title='r2d2_redis::RedisConnectionManager'>RedisConnectionManager</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

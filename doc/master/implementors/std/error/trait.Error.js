675cb4749f5243f7772a1a8d54b292b7e7c554f0
Simo Kinnunen (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) <simo@shoqolate.com>
2016-05-25 16:23:19 +0000

    Rebuild pages at a607296c06d9e91ab2fb4877342a511f14a87e99

--- doc/master/implementors/std/error/trait.Error.js
+++ doc/master/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['r2d2_redis'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='r2d2_redis/enum.Error.html' title='r2d2_redis::Error'>Error</a>",];
+implementors["r2d2_redis"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='r2d2_redis/enum.Error.html' title='r2d2_redis::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

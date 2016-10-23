1c1dd6eec9db5fb48bc4422d258f6683f600c88c
Simo Kinnunen (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) <simo@shoqolate.com>
2016-10-23 04:33:14 +0000

    Rebuild pages at 8a4e921a49e336ed02d0aa21ebc1e32c86469ec2

--- doc/master/implementors/r2d2/trait.ManageConnection.js
+++ doc/master/implementors/r2d2/trait.ManageConnection.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["r2d2_redis"] = ["impl <a class='trait' href='https://sfackler.github.io/r2d2/doc/v0.7.0/r2d2/trait.ManageConnection.html' title='r2d2::ManageConnection'>ManageConnection</a> for <a class='struct' href='r2d2_redis/struct.RedisConnectionManager.html' title='r2d2_redis::RedisConnectionManager'>RedisConnectionManager</a>",];
+implementors["r2d2_redis"] = ["impl <a class='trait' href='https://sfackler.github.io/r2d2/doc/v0.7.1/r2d2/trait.ManageConnection.html' title='r2d2::ManageConnection'>ManageConnection</a> for <a class='struct' href='r2d2_redis/struct.RedisConnectionManager.html' title='r2d2_redis::RedisConnectionManager'>RedisConnectionManager</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

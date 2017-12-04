d01d3cbf2465a0d227d7457c0a41c64368f508f6
Simo Kinnunen (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) (via Travis CI) <simo@shoqolate.com>
2017-12-04 21:38:35 +0000

    Release v0.7.0

---
+++ doc/v0.7.0/implementors/r2d2/trait.ManageConnection.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["r2d2_redis"] = ["impl <a class=\"trait\" href=\"https://docs.rs/r2d2/0.8/r2d2/trait.ManageConnection.html\" title=\"trait r2d2::ManageConnection\">ManageConnection</a> for <a class=\"struct\" href=\"r2d2_redis/struct.RedisConnectionManager.html\" title=\"struct r2d2_redis::RedisConnectionManager\">RedisConnectionManager</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

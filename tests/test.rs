a74e31a8bfe379f02f61a8eba088bb884ae41511
Viktor Kuroljov <viktor.konsta@gmail.com>
2020-01-12 11:26:11 +0200

    Add test_threaded_ping

--- tests/test.rs
+++ tests/test.rs
@@ -64,3 +64,26 @@ fn test_counter() {
     let n: i64 = conn.incr("counter", 1).unwrap();
     assert_eq!(n, 42);
 }
+
+#[test]
+fn test_threaded_ping() {
+    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .unwrap();
+
+    let mut handles = vec![];
+
+    for _i in 0..10i32 {
+        let pool = pool.clone();
+        handles.push(thread::spawn(move || {
+            let mut conn = pool.get().unwrap();
+            let reply = redis::cmd("PING").query::<String>(&mut *conn).unwrap();
+            assert_eq!("PONG", reply);
+        }));
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
